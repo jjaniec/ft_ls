41c3de806f6fe073a907bba297eed526855eeaff
Joffrey Janiec <jjaniec@student.42.fr>
2018-03-12 17:26:57 +0100

    fix some leaks

--- srcs/ft_colorize_name.c
+++ srcs/ft_colorize_name.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/09 18:34:37 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/03/09 18:47:48 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/03/12 17:22:26 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,18 @@
 
 void	ft_colorize_name(t_str_stats *f)
 {
+	char	*s;
+
 	if (f->folder || f->perms[3] == 'x')
 	{
+		s = f->name;
 		if (f->folder)
 			f->name = ft_strjoin(DIR_COLOR, f->name);
 		else
 			f->name = ft_strjoin(EXEC_COLOR, f->name);
+		free(s);
+		s = f->name;
 		f->name = ft_strjoin(f->name, FG_WHITE);
+		free(s);
 	}
-
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 21:53:10 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/03/12 14:49:51 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/03/12 17:19:51 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void		ft_init_args(int ac, char **av, t_args *args)
 	t_opt	*opts;
 	t_param	*params;
 
-	opts = NULL;
 	params = NULL;
 	opts = (ac > 1) ? (ft_parse_options(ac, av)) : (NULL);
 	if (opts)
@@ -46,20 +45,28 @@ void	ft_ls_foreach_in_dir(char *s, t_opt *opts)
 	struct dirent 	*dir;
 	t_str_stats		*st;
 	char			*ns;
+	size_t			l;
 
 	d = opendir(s);
 	if (d)
 	{
+		ft_printf("%s:\n", s);
 		dir = readdir(d);
 		dir = readdir(d);
-		while ((dir = readdir(d)) != NULL)
+		while ((dir = readdir(d)))
 		{
-			printf("%s\n", s);
-			ns = ft_strjoin(ft_strjoin(s, "/"), dir->d_name);
+			l = ft_strlen(s);
+			ns = ft_strnew(l + 1);
+			ft_strcpy(ns, s);
+			ns[l] = '/';
+			ns = ft_strjoin_free(ns, ft_strdup(dir->d_name));
 			st = ft_get_stats(ns, opts);
 			ft_debug_str_stats(ns, st, opts);
-			if (st && st->folder && opts && opts->r_caps && st->name[0] != '.' && s[0] != '.')
+			//ft_printf("%s\n", ns);
+			if (st && st->folder && opts && opts->r_caps)
 				ft_ls_foreach_in_dir(ns, opts);
+			free(ns);
+			free(st);
 		}
 		closedir(d);
 	}
@@ -73,6 +80,7 @@ void	ft_ls_foreach_in_dir(char *s, t_opt *opts)
 void	ft_ls(t_args args)
 {
 	t_param		*aptr;
+	t_param		*prev;
 	t_str_stats	*infs;
 
 	aptr = args.prm;
@@ -82,7 +90,10 @@ void	ft_ls(t_args args)
 		ft_debug_str_stats(aptr->s, infs, args.opt);
 		if (infs && infs->folder)
 			ft_ls_foreach_in_dir(aptr->s, args.opt);
+		prev = aptr;
 		aptr = aptr->next;
+		free(prev);
+		free(infs);
 	}
 }
 
