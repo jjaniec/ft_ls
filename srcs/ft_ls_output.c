a8e4a3e9c66118c87e6d761e77f39b625fdbb896
Joffrey Janiec <jjaniec@student.42.fr>
2018-03-22 15:22:03 +0100

    fix beginning of path being '//' with ft_ls -R /

--- srcs/ft_ls_output.c
+++ srcs/ft_ls_output.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 14:32:39 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/03/21 19:12:46 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/03/22 15:15:55 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ void	ft_ls_output_entry(t_str_stats *de, t_opt *opts)
 	if (opts && de)
 	{
 		if (opts->l && !opts->n)
-			PRINTF("%s%c %2d %-7s %-8s %8d %s %s\n", de->perms, \
+			PRINTF("%s%c %3d %-7s %-8s %8d %s %s\n", de->perms, \
 				de->perms_attr_acl, de->slnks, de->ownr, \
 				de->ownr_grp, de->size, de->last_mod, de->name);
 		if (opts->l && opts->n)
-			PRINTF("%s%c %2d %-5u %-4u %8d %s %s\n", de->perms, \
+			PRINTF("%s%c %3d %-5u %-4u %8d %s %s\n", de->perms, \
 				de->perms_attr_acl, de->slnks, de->ownr_uid, \
 				de->ownr_grp_uid, de->size, de->last_mod, de->name);
 	}
