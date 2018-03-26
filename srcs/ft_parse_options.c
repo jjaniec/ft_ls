aa09a530d2357b5bb93fcdf9965bc7f6221a17f6
Joffrey Janiec <jjaniec@student.42.fr>
2018-03-26 20:24:46 +0200

    throw error when option is specified without spaces like '-l-G'

--- srcs/ft_parse_options.c
+++ srcs/ft_parse_options.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 21:53:41 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/03/26 17:47:34 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/03/26 20:23:32 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static void		ft_toggle_opt(t_opt *opts, char *str, char *pname)
 {
 	while (*(++str))
 	{
-		if (!ft_is_option(str))
+		if ((*str == '-' && str[-1] != '-') || !ft_is_option(str))
 			ft_handle_opt_err(*str, pname);
 		if (ft_strcmp("-linux-sort", str) == 0)
 		{
