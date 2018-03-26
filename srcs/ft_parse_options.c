ebfd1042c8f94e89ca27bdbe03550d4986ef0806
Joffrey Janiec <jjaniec@student.42.fr>
2018-03-26 17:49:18 +0200

    add option --color to toggle -G macos option

--- srcs/ft_parse_options.c
+++ srcs/ft_parse_options.c
@@ -6,7 +6,7 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 21:53:41 by jjaniec           #+#    #+#             */
-/*   Updated: 2018/03/26 17:28:36 by jjaniec          ###   ########.fr       */
+/*   Updated: 2018/03/26 17:47:34 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,12 @@ static void		ft_toggle_opt(t_opt *opts, char *str, char *pname)
 			opts->n = TRUE;
 			opts->l = TRUE;
 		}
-		if (*str == 'G')
+		if (*str == 'G' || ft_strcmp("-color", str) == 0)
+		{
 			opts->g_caps = TRUE;
+			if (*str != 'G')
+				break;
+		}
 	}
 }
 
